f7ca5458427ac75a1cc8916994c9142d30be69f4
Max_Arkhipov <maarchipov@gmail.com>
2025-03-27 22:30:03 +0500

    Add Docker

--- src/config.py
+++ src/config.py
@@ -9,10 +9,12 @@ DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
-SMTP_USER = os.getenv("SMTP_USER")
 
-SECRET = "YOUR_SECRET_HERE"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# import os, base64
-# print(base64.urlsafe_b64encode(os.urandom(32)).decode())
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+REDIS_DB = os.getenv("REDIS_DB", "0")
+REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+
+SECRET = os.getenv("SECRET", "YOUR_SECRET_HERE")
